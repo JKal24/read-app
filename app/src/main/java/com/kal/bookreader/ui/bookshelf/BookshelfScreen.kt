e10f9be6045163a0d91d58aaacdd5934284f28c3
JKal24 <jkalirai2499@gmail.com>
2023-07-10 23:49:32 -0400

    Added coil to view images

--- app/src/main/java/com/kal/bookreader/ui/bookshelf/BookshelfScreen.kt
+++ app/src/main/java/com/kal/bookreader/ui/bookshelf/BookshelfScreen.kt
@@ -1,17 +1,27 @@
 package com.kal.bookreader.ui.bookshelf
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.kal.bookreader.R
 import com.kal.bookreader.data.entity.Book
+import java.io.File
+import java.nio.file.Paths
 
 @Composable
 fun GetAndDisplayBooks(bookshelfViewModel: BookshelfViewModel = viewModel()) {
@@ -20,7 +30,10 @@ fun GetAndDisplayBooks(bookshelfViewModel: BookshelfViewModel = viewModel()) {
     Scaffold(
         scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed)),
         content = { innerPadding ->
-            Box (Modifier.fillMaxHeight().padding(innerPadding)) {
+            Box (
+                Modifier
+                    .fillMaxHeight()
+                    .padding(innerPadding)) {
                 Column(
                     Modifier
                         .fillMaxWidth()
@@ -52,16 +65,40 @@ fun GetAndDisplayBooks(bookshelfViewModel: BookshelfViewModel = viewModel()) {
 
 @Composable
 fun DisplayBook(book : Book) {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .clickable(onClick = {
+    Box(
+        Modifier.fillMaxWidth()
+    ) {
+        val context by rememberUpdatedState(LocalContext.current)
+        val imageSrc = getStorageBookImageFile(context, book.book_src, book.image_src)
 
-            }), horizontalArrangement = Arrangement.SpaceEvenly) {
-            Text(text = book.book_src)
-            Column {
-                Text(text = book.book_name)
-                Text(text = book.image_src)
-            }
+        val bookImageResource = ImageRequest.Builder(context)
+            .data(imageSrc)
+            .crossfade(true)
+            .build()
+
+        val imageErrorResource = ImageRequest.Builder(context)
+            .data(null)
+            .crossfade(true)
+            .build()
+
+        AsyncImage(
+            model = bookImageResource,
+            contentDescription = book.book_name,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier,
+            error = rememberAsyncImagePainter(
+                model = imageErrorResource,
+                contentScale = ContentScale.Fit
+            )
+        )
     }
+}
+
+fun getStorageBookImageFile(context: Context, bookPath: String, imagePath: String): File {
+    val folderBooks = File(context.filesDir, "books")
+    return Paths.get(
+        folderBooks.absolutePath,
+        bookPath.removePrefix("local://"),
+        imagePath.removePrefix("local://")
+    ).toFile()
 }
